9efb09749b85acfbc2299fb6dec6146b942c6b2e
Yann Collet <cyan@fb.com>
2025-01-18 20:19:55 -0800

    added a CI test for x86 32-bit + avx2 combination
    which is expected to be quite rare, but nonetheless possible.
    
    This test is initially expected to fail, before integration of #4248 fix

--- lib/common/compiler.h
+++ lib/common/compiler.h
@@ -210,7 +210,7 @@
 /*Like DYNAMIC_BMI2 but for compile time determination of BMI2 support*/
 #ifndef STATIC_BMI2
 #  if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_I86))
-#    ifdef __AVX2__  //MSVC does not have a BMI2 specific flag, but every CPU that supports AVX2 also supports BMI2
+#    ifdef __AVX2__  /* MSVC does not have a BMI2 specific flag, but every CPU that supports AVX2 also supports BMI2 */
 #       define STATIC_BMI2 1
 #    endif
 #  elif defined(__BMI2__) && defined(__x86_64__) && defined(__GNUC__)
